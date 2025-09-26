7edae8b033740cff9ef11d2570b9af674916f4e9
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-09-26 14:55:54 +0600

    ok

---
+++ src/main.java
@@ -0,0 +1,17 @@
+
+
+public class main {
+
+    public static void main(String[] args) {
+
+            System.out.println("Hello and welcome!");
+
+            for (int i = 1; i <= 5; i++) {
+
+                System.out.println("i = " + i);
+
+
+            }
+        }
+    }
+
